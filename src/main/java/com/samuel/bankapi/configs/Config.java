b98401a5162454678481c9e2de7a286b09e1f375
samieIzone5 <samuel@izone5.com.ng>
2024-10-05 00:11:25 +0000

    fixed dependency injection bug

---
+++ src/main/java/com/samuel/bankapi/configs/Config.java
@@ -0,0 +1,15 @@
+package com.samuel.bankapi.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
