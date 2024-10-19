0f4a19d9f0cf025b6c2f3d6d434103eb4d3df648
samieIzone5 <samuel@izone5.com.ng>
2024-10-19 12:27:13 +0000

    updated cors config

--- src/main/java/com/samuel/bankapi/configs/CorsConfig.java
+++ src/main/java/com/samuel/bankapi/configs/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Adjust the path as needed
-                .allowedOrigins("http://localhost:8081", "exp://192.168.1.45:8081", "http://localhost:5173", "https://bank-website-git-theming-samieosehs-projects.vercel.app") // Add your allowed origins
+                .allowedOrigins("http://localhost:8081", "exp://192.168.1.45:8081", "http://localhost:5173",
+                        "https://bank-website-panel.vercel.app/") // Add your allowed origins
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders(
                        "Authorization",
